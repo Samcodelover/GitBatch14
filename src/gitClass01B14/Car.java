7c66790abefa732198c5491d12c29ffd04201923
Samcodelover <samal010590@gmail.com>
2022-09-30 22:36:30 -0400

    second commit

--- src/gitClass01B14/Car.java
+++ src/gitClass01B14/Car.java
@@ -5,8 +5,8 @@ public class Car {
 	String color="White";
 	String make="Tesla";
 	String model="S3";
-	int year =2018;
-	int Hp=300;
+	int year =2020;
+	
 	
 void moveForward() {
 	System.out.println("Moving Forwards....");
